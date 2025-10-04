79755cf3b0c086e7de0804bf78990248231849f2
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2025-10-04 07:39:06 +0200

    Add new duration model

--- src-tauri/src/domains/tracker/domain/model.rs
+++ src-tauri/src/domains/tracker/domain/model.rs
@@ -36,8 +36,8 @@ impl fmt::Display for TrackerEntry {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "TrackerEntry(id: {}, label: {}, created_at: {}, updated_at: {})",
-            self.id, self.label, self.created_at, self.updated_at
+            "TrackerEntry(id: {}, label: {}, created_at: {}, updated_at: {}, is_deleted: {})",
+            self.id, self.label, self.created_at, self.updated_at, self.is_deleted
         )
     }
 }
@@ -47,27 +47,17 @@ pub struct TrackerEntryLine {
     pub id: i64,
     pub entry_id: i64,
     pub desc: String,
-    pub started_at: DateTime<Utc>,
-    pub ended_at: Option<DateTime<Utc>>,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
     pub is_deleted: bool,
 }
 
 impl TrackerEntryLine {
-    pub fn new(
-        id: i64,
-        entry_id: i64,
-        desc: String,
-        started_at: DateTime<Utc>,
-        ended_at: Option<DateTime<Utc>>,
-    ) -> Self {
+    pub fn new(id: i64, entry_id: i64, desc: String) -> Self {
         Self {
             id,
             entry_id,
             desc,
-            started_at,
-            ended_at,
             ..Default::default()
         }
     }
@@ -79,8 +69,6 @@ impl Default for TrackerEntryLine {
             id: 0,
             entry_id: 0,
             desc: String::new(),
-            started_at: Utc::now(),
-            ended_at: None,
             created_at: Utc::now(),
             updated_at: Utc::now(),
             is_deleted: false,
@@ -92,14 +80,66 @@ impl fmt::Display for TrackerEntryLine {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "TrackerEntryLine(id: {}, entry_id: {}, desc: {}, started_at: {}, ended_at: {:?}, created_at: {}, updated_at: {})",
+            "TrackerEntryLine(id: {}, entry_id: {}, desc: {}, created_at: {}, updated_at: {}, is_deleted: {})",
+            self.id, self.entry_id, self.desc, self.created_at, self.updated_at, self.is_deleted
+        )
+    }
+}
+
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize, sqlx::FromRow)]
+pub struct TrackerEntryLineDuration {
+    pub id: i64,
+    pub entry_line_id: i64,
+    pub started_at: DateTime<Utc>,
+    pub ended_at: Option<DateTime<Utc>>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub is_deleted: bool,
+}
+
+impl TrackerEntryLineDuration {
+    pub fn new(
+        id: i64,
+        entry_line_id: i64,
+        started_at: DateTime<Utc>,
+        ended_at: Option<DateTime<Utc>>,
+    ) -> Self {
+        Self {
+            id,
+            entry_line_id,
+            started_at,
+            ended_at,
+            ..Default::default()
+        }
+    }
+}
+
+impl Default for TrackerEntryLineDuration {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            entry_line_id: 0,
+            started_at: Utc::now(),
+            ended_at: None,
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+            is_deleted: false,
+        }
+    }
+}
+
+impl fmt::Display for TrackerEntryLineDuration {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "TrackerEntryLineDuration(id: {}, entry_line_id: {}, started_at: {}, ended_at: {:?}, created_at: {}, updated_at: {}, is_deleted: {})",
             self.id,
-            self.entry_id,
-            self.desc,
+            self.entry_line_id,
             self.started_at,
             self.ended_at,
             self.created_at,
-            self.updated_at
+            self.updated_at,
+            self.is_deleted
         )
     }
 }
